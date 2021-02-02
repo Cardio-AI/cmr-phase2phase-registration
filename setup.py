ec999c298da0dbe621af21f551e1ec93b1c7b875
sven hd <emailsvenkoehler@gmx.net>
2021-02-02 15:19:21 +0100

    init

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Extract the cardiac motion from dynamic CMR images.',
+    author='svenkoehler',
+    license='',
+)
