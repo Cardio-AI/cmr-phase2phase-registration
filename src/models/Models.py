14bceb36855804ee7cc4e5254d05bc5b193da2da
sven hd <emailsvenkoehler@gmx.net>
2022-02-07 11:35:40 +0100

    added minmax resacling of the direction

--- src/models/Models.py
+++ src/models/Models.py
@@ -451,7 +451,7 @@ def create_PhaseRegressionModel_v2(config, networkname='PhaseRegressionModel'):
 
         if add_bilstm:
             # min/max normlisation as lambda
-            #minmax_lambda = lambda x: (x - tf.reduce_min(x)) / (tf.reduce_max(x) - tf.reduce_min(x) + keras.backend.epsilon())
+            minmax_lambda = lambda x: (x - tf.reduce_min(x)) / (tf.reduce_max(x) - tf.reduce_min(x) + keras.backend.epsilon())
 
             """flow_features = keras.layers.BatchNormalization()(flow_features)
             flow_features = keras.layers.Conv1D(filters=16,kernel_size=3, padding='same')(flow_features)
@@ -460,7 +460,10 @@ def create_PhaseRegressionModel_v2(config, networkname='PhaseRegressionModel'):
             print('Shape before LSTM layers: {}'.format(flow_features.shape))
             #flow_features = tf.map_fn(minmax_lambda,flow_features) # per instance rescaling
 
-            #flow_features = minmax_lambda(flow_features)
+            #flow_features = tf.keras.layers.LayerNormalization(axis=-1)(flow_features)
+
+            flow_features = minmax_lambda(flow_features)
+            #flow_features = keras.layers.BatchNormalization()(flow_features)
             flow_features = bi_lstm_layer(flow_features)
             flow_features = keras.layers.Dropout(rate=0.2)(flow_features)
             flow_features = bi_lstm_layer1(flow_features)
